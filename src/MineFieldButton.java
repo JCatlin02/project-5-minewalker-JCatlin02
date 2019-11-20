27363d22251fd31bc011af6aff1d37feab1c11a7
Jon <54995313+JCatlin02@users.noreply.github.com>
2019-11-20 11:09:21 -0700

    Set up MineWalker JFrame

---
+++ src/MineFieldButton.java
@@ -0,0 +1,12 @@
+import javax.swing.JButton;
+
+public class MineFieldButton extends JButton{
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/MineFieldPanel.java
@@ -0,0 +1,12 @@
+import javax.swing.JPanel;
+
+public class MineFieldPanel extends JPanel{
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
