247cbc083427f7ce14104173c751088cfd7614ff
Jon <54995313+JCatlin02@users.noreply.github.com>
2019-11-22 11:01:06 -0700

    all buttons working and MineFieldPanel works well

--- src/MineFieldButton.java
+++ src/MineFieldButton.java
@@ -1,11 +1,40 @@
 import javax.swing.JButton;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
+import java.awt.Color;
 public class MineFieldButton extends JButton{
-
+	private Boolean isPath;
+	private Boolean isMine;
+	
 	MineFieldButton(ActionListener listener){
 		addActionListener(listener);
 		setPreferredSize(new Dimension(50, 50));
+		isMine = false;
+		isPath = false;
+		setBackground(Color.white);
+	}
+	
+	public void makeMine() {
+		isMine = true;
+	}
+	public void makePath() {
+		isPath = true;
+	}
+	
+	
+	public void notMine() {
+		isMine = false;
+	}
+	public void notPath() {
+		isPath = false;
+	}
+	
+	
+	public Boolean isMine() {
+		return isMine;
+	}
+	public Boolean isPath() {
+		return isPath;
 	}
 	
 	/**
--- src/MineFieldPanel.java
+++ src/MineFieldPanel.java
@@ -5,6 +5,7 @@ import java.awt.Point;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+@SuppressWarnings("serial")
 public class MineFieldPanel extends JPanel{
 	public RandomWalk minePath;
 	//ArrayList used for creating the walk
@@ -14,6 +15,8 @@ public class MineFieldPanel extends JPanel{
 	JButton[][] spaces;
 	//keeps track of how many mines there are.
 	int mines = 0;
+	//width
+	int width;
 	/**
 	 * Constructor for the MineField.
 	 * @param listener
@@ -21,42 +24,44 @@ public class MineFieldPanel extends JPanel{
 	 * @param height
 	 */
 	MineFieldPanel(ActionListener listener, int width, int height) {
+		this.width = width;
 		
 		//creates a RandomWalk
 		minePath = new RandomWalk(width);
 		minePath.createWalk();
+		
 		//puts the randomWalks arrayList into actualPath so we can use the ArrayList methods.
 		actualPath = minePath.getPath();
+		
 		//instantiates the spaces and sets the grid layout
 		spaces = new JButton[width][height];
 		setLayout(new GridLayout(width, height));
+		
 		/**
 		 * creates the mine field. adds button to spaces, then spaces to field and sets the background to white.
 		 * then, it adds each space to the spaces ArrayList so that we can use it to see the RandomWalk.
 		 */
 		for(int i = 0; i < spaces.length; i++) {
-			for(int j = 0; j<spaces[i].length; j++) {
+			for(int j = 0; j< spaces[i].length; j++) {
 				spaces[i][j] = new MineFieldButton(listener);
 				add(spaces[i][j]);
-				spaces[i][j].setBackground(Color.white);
 			}
 		}
 		
-		
-		//Uses the RandomWalk path to call spaces and make them blue.
+		// makes the entire path (does not show them)
 		for (int i = 0; i < actualPath.size(); i++) {
-			spaces[(int)actualPath.get(i).getX()][(int)actualPath.get(i).getY()].setBackground(Color.blue);
+			((MineFieldButton) spaces[(int)actualPath.get(i).getX()][(int)actualPath.get(i).getY()]).makePath();
 		}
 		
 		//only 25% of the remaining mines should be black. there are 15 spaces in the actualPath, so (64-15)/4 = mines. also = 13(rounded up.)
-		//places mines outside actualPath.
+		//places mines outside actualPath, does not show them.
 		while (mines < 13) {
 			for(int i = 0; i < spaces.length; i++) {
 				for(int k = 0; k < spaces[i].length; k++) {
-					if(spaces[i][k].getBackground() != Color.blue && spaces[i][k].getBackground() != Color.black) {
+					if(!((MineFieldButton) spaces[i][k]).isPath() && !((MineFieldButton) spaces[i][k]).isMine()) {
 						int rand1 = rand.nextInt(13);
 						if (rand1 == 1 && mines < 13) {
-							spaces[i][k].setBackground(Color.black);
+							((MineFieldButton) spaces[i][k]).makeMine();
 							mines++;
 						}
 					}
@@ -64,20 +69,68 @@ public class MineFieldPanel extends JPanel{
 			}
 		}
 	}
-	
-	public void showPath() {
-		
-	}
-	
-	public void showMines() {
-		
-	}
-	void reset() {
-	//clears mineWalker	
-	}
-	/**
-	 * 
+	/*
+	 * colors all the mines blue
 	 */
-	private static final long serialVersionUID = 1L;
-
+	public void showPath() {
+		for (int i = 0; i < actualPath.size(); i++) {
+			spaces[(int)actualPath.get(i).getX()][(int)actualPath.get(i).getY()].setBackground(Color.blue);
+		}
+	}
+	/*
+	 * colors all the mines black
+	 */
+	public void showMines() {
+		for(int i = 0; i < spaces.length; i++) {
+			for(int k = 0; k < spaces[i].length; k++) {
+				if(((MineFieldButton) spaces[i][k]).isMine()) {
+				
+						spaces[i][k].setBackground(Color.black);
+				}
+			}
+		}
+	}
+	
+	void reset() {
+		/*
+		 * actual resetting
+		 */
+		mines = 0;
+		for(int i = 0; i < spaces.length; i++) {
+			for(int k = 0; k < spaces[i].length; k++) {
+				((MineFieldButton)spaces[i][k]).setBackground(Color.white);
+				((MineFieldButton)spaces[i][k]).notMine();
+				((MineFieldButton)spaces[i][k]).notPath();
+			}
+		}
+		/*
+		 * makes new randomWalk
+		 */
+		minePath = new RandomWalk(width);
+		minePath.createWalk();
+		//puts the randomWalks arrayList into actualPath so we can use the ArrayList methods.
+		actualPath = minePath.getPath();
+		/*
+		 * makes new path
+		 */
+		for (int i = 0; i < actualPath.size(); i++) {
+			((MineFieldButton)spaces[(int)actualPath.get(i).getX()][(int)actualPath.get(i).getY()]).makePath();
+		}
+		/*
+		 * makes new mines
+		 */
+		while (mines < 13) {
+			for(int i = 0; i < spaces.length; i++) {
+				for(int k = 0; k < spaces[i].length; k++) {
+					if(!((MineFieldButton) spaces[i][k]).isPath() && !((MineFieldButton) spaces[i][k]).isMine()) {
+						int rand1 = rand.nextInt(13);
+						if (rand1 == 1 && mines < 13) {
+							((MineFieldButton) spaces[i][k]).makeMine();
+							mines++;
+						}
+					}
+				}
+			}
+		}
+	}
 }
