e2256d23198de87287eb1db05af85cc938b77164
Jon <54995313+JCatlin02@users.noreply.github.com>
2019-11-21 21:41:31 -0700

    CenterAlign buttons

--- src/UserButtonPanel.java
+++ src/UserButtonPanel.java
@@ -1,3 +1,4 @@
+import java.awt.Component;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
@@ -11,12 +12,15 @@ public class UserButtonPanel extends JPanel {
 		//adds a reset button (that will appear outside the MineFieldPanel) and adds an actionListener for it
 			reset = new JButton("reset");
 			reset.addActionListener(listener1);
+			reset.setAlignmentX(Component.CENTER_ALIGNMENT);
 			//adds showMines button, aligns it, and adds listener for it.
 			showMines = new JButton("Show Mines");
 			showMines.addActionListener(listener3);
+			showMines.setAlignmentX(Component.CENTER_ALIGNMENT);
 			//adds showPath button, aligns it, and adds listener for it.
 			showPath = new JButton("Show Path");
 			showPath.addActionListener(listener2);
+			showPath.setAlignmentX(Component.CENTER_ALIGNMENT);
 			//adds panel and reset button to MineWalkerPanel.
 			add(reset);
 			add(showMines);
